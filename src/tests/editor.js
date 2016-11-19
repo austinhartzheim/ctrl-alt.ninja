8faf18d3ca459cc366583fdcf3c205a89e5d0264
Kristina Fischer <kmfischer3@wisc.edu>
2016-11-19 15:37:25 -0600

    adding unit tests for Home and End

--- src/tests/editor.js
+++ src/tests/editor.js
@@ -53,6 +53,49 @@ describe('test Editor', function() {
         expect(editor.cursor_pos_y).toBe(1);
     });
 
+    it('test home on line 0', function() {
+        var editor = new Editor(null);
+        editor.set_data_buffer(['hello']);
+
+        editor.move_cursor(1, 0);
+        editor.move_cursor_home();
+
+        expect(editor.cursor_pos_x).toBe(0);
+        expect(editor.cursor_pos_y).toBe(0);
+    });
+
+    it('test home on last line', function() {
+        var editor = new Editor(null);
+        editor.set_data_buffer(['hello', 'world']);
+
+        editor.move_cursor(3, 1);
+        editor.move_cursor_home();
+
+        expect(editor.cursor_pos_x).toBe(0);
+        expect(editor.cursor_pos_y).toBe(1);
+    });
+
+    it('test end on line 0', function() {
+        var editor = new Editor(null);
+        editor.set_data_buffer(['hello']);
+
+        editor.move_cursor_end();
+
+        expect(editor.cursor_pos_x).toBe(5);
+        expect(editor.cursor_pos_y).toBe(0);
+    });
+
+    it('test end on last line', function() {
+        var editor = new Editor(null);
+        editor.set_data_buffer(['hello', 'world']);
+
+        editor.move_cursor(2, 1);
+        editor.move_cursor_end();
+
+        expect(editor.cursor_pos_x).toBe(5);
+        expect(editor.cursor_pos_y).toBe(1);
+    });
+
     it('test backspace', function() {
         var editor = new Editor(null);
         editor.set_data_buffer(['hello']);
