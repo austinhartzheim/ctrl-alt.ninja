73914c92ac2eba729e008de7cef495eb10f0eb53
Austin Hartzheim <inbox@austinhartzheim.me>
2016-11-19 07:48:16 +0000

    Implemented basic text rendering.

--- src/js/main.js
+++ src/js/main.js
@@ -6,13 +6,23 @@ function Game() {
 /*
  * Manages a visual editor display in the page.
  * Exposes an interface to allow KeyboardLayouts to interact with the editor.
+ * elm: the element containing the editor interface
  */
-function Editor() {
+function Editor(elm) {
+    this.elm = elm;
+    
     this.cursor_pos_x = 0;
     this.cursor_pos_y = 0;
 
-    this.data_buffer = ["hello world"];
+    this.data_buffer = ["hello world", "how are you?"];
 }
+
+Editor.prototype.render = function() {
+    this.elm.empty();
+    for (var i = 0; i < this.data_buffer.length; i++) {
+        this.elm.append("<p>" + this.data_buffer[i] + "</p>");
+    }
+};
 
 /*
  * Shift the current positiono of the cursor.
@@ -39,19 +49,25 @@ Editor.prototype.move_cursor_home = function() {
  */
 Editor.prototype.type_character = function(c) {
     if (c == '\n') {
-        var pre_cursor = this.data_buffer[this.cursor_pos_y].splice(0, this.cursor_pos_x);
-        var post_cursor = this.data_buffer[this.cursor_pos_y].splice(this.cursor_pos_x);
-        this.data_buffer[this.cursor_pos_y] = pre_cursor;
-        this.data_buffer.splice(this.cursor_pos_y + 1, 0, post_cursor);
-
-        // Move cursor to the start of the next line
-        this.cursor_pos_x = 0;
-        this.cursor_pos_y++;
-
+        this.type_newline();
         return;
     }
 
     this.data_buffer[this.cursor_pos_y] = utils.string_insert(
         this.data_buffer[this.cursor_pos_y], this.cursor_pos_x, c
     );
+};
+
+/*
+ * Insert a newline at cursor position.
+ */
+Editor.prototype.type_newline = function() {
+    var pre_cursor = this.data_buffer[this.cursor_pos_y].splice(0, this.cursor_pos_x);
+    var post_cursor = this.data_buffer[this.cursor_pos_y].splice(this.cursor_pos_x);
+    this.data_buffer[this.cursor_pos_y] = pre_cursor;
+    this.data_buffer.splice(this.cursor_pos_y + 1, 0, post_cursor);
+    
+    // Move cursor to the start of the next line
+    this.cursor_pos_x = 0;
+    this.cursor_pos_y++;
 };
