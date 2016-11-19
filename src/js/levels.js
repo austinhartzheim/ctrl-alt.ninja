58b68bfb9904efd4cb356784bedf387055f3fd56
Austin Hartzheim <inbox@austinhartzheim.me>
2016-11-19 20:54:19 +0000

    Implemented main loop with equality checking.

--- src/js/levels.js
+++ src/js/levels.js
@@ -1,7 +1,16 @@
 function Level1() {
     this.display = new Editor($('#text-display'));
+    this.display.set_data_buffer(['match this text']);
+    this.display.render();
     
     this.editor = new Editor($('#text-editor'));
+    this.editor.set_data_buffer(['start with this']);
     this.keyboard_layout = new KeyboardLayout(this.editor);
     this.editor.render();
 }
+
+Level1.prototype.loop = function() {
+    if (this.display.equals(this.editor)) {
+        alert('you win the game!');
+    }
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,18 @@
 function Game() {
     this.level = new Level1();
+
+    this.animation_frame_request = null;
+    this.loop_interval = 10;
 }
+
+Game.prototype.start = function() {
+    setTimeout(function() {game.loop();}, this.loop_interval);
+};
+
+Game.prototype.loop = function(timestamp) {
+    this.level.loop();
+    setTimeout(function() {game.loop();}, this.loop_interval);
+};
 
 /*
  * Manages a visual editor display in the page.
