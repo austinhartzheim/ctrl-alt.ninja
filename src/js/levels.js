8e1a87b89dcb7ac13b55f46759a15c1e7da2ce88
Austin Hartzheim <inbox@austinhartzheim.me>
2016-11-20 15:20:21 +0000

    Add some set-up for level 1.

--- src/js/levels.js
+++ src/js/levels.js
@@ -50,10 +50,13 @@ var level_data = [
     },
 
     {
-        title: 'Leaving Home',
-        desc_short: 'Don\'t be in such a rush.',
-        desc_long: 'Use ctrl+e to go to the end of a line. Press ctrl+a to go to the start of a line. The home and end keys can also be used.',
-        msg_win: 'TODO: edit me',
+        title: 'Teleporting Home',
+        desc_short: 'They won\'t see you coming.',
+        desc_long: ('<h2>Let\'s start with the basics</h2>' +
+                    '<p>The first skill every ninja-in-training learns is how to quickly move the cursor to the start and end of a line. With one keypress you can easily dazzle your foes and appear to teleport through them before landing your final blow.</p>' +
+                    '<p>(Of course, by the time you finish your training, they won\'t be able to see you at all!)</p>' +
+                    '<h2>New shortcuts!</h2><dl><dt>Ctrl+a</dt><dd>Skip to the start of a link.</dd><dt>Ctrl+e</dt><dd>Skip to the end of a line.</dd><dt>Alt+&lt;</dt><dd>Skip to the start of the file.</dd><dt>Alt+&gt;</dt><dd>Skip to the end of the file.</dd></dl>'),
+        msg_win: '<h2>You did it!</h2><p>Can you feel the ninja power starting to take root in your fingers?</p>',
         steps: [
             {
                 start: ['What on earth am I going to do with this long line?'],
@@ -277,7 +280,7 @@ Level1.prototype.win = function() {
                'You passed a level.',
                level_data[this.num].msg_win,
                'Continue',
-               function(e) {console.log('not implemented');}
+               function(e) {start_level_2()}
               );
     // TODO: allow advancing to the next level
 };
@@ -288,6 +291,118 @@ function start_level_1() {
     game = new Game(Level1);
     game.start();
 }
+
+
+
+
+/*
+ * Level 2
+ */
+function Level2() {
+    this.num = 2;
+
+    this.steps = level_data[this.num].steps;
+    this.progress = 0;
+
+    this.intro();
+    this.set_up_level();
+}
+
+Level2.prototype.start = function() {
+    $('html, body').animate({
+        scrollTop: $("#level2").offset().top
+    }, 750, function() {
+        game.level.intro();
+    });
+};
+
+Level2.prototype.intro = function() {
+    show_modal(level_data[this.num].title,
+               level_data[this.num].desc_short,
+               level_data[this.num].desc_long,
+               'Let\'s Start!',
+               function(e) {game.level.set_up_level();}
+              );
+};
+
+Level2.prototype.set_up_level = function() {
+    // Detach old keyboard bindings if any
+    if (this.keyboard_layout != null) {
+        this.keyboard_layout.destruct();
+    }
+
+    // Check if there is a next step; return early
+    if (this.progress >= this.steps.length) {
+        this.win();
+        return;
+    }
+
+    // Store/Set up the cursor state
+    var pos = {};
+    switch(this.steps[this.progress].pos.mode) {
+    case MODES.KEEP:
+        pos.x = this.editor.get_cursor_x();
+        pos.y = this.editor.get_cursor_y();
+        break;
+    case MODES.SET:
+        pos.x = this.steps[this.progress].pos.x;
+        pos.y = this.steps[this.progress].pos.y;
+        break;
+    default:
+        throw 'No cursor position mode set';
+    }
+    
+    // Create editors
+    this.display = new Editor($('#level2-display'), false);
+    this.editor = new Editor($('#level2-editor'), true);
+
+    // Initialize editor states
+    console.log(this.display);
+    this.display.set_data_buffer(
+        this.steps[this.progress]['match']
+    );
+    this.editor.set_data_buffer(
+        this.steps[this.progress]['start']
+    );    
+    this.editor.set_cursor(pos.x, pos.y);
+    this.editor.enable_diffing(this.display);
+    this.display.render();
+    this.editor.render();
+
+    // Attach keyboard binding
+    this.keyboard_layout = new KeyboardLayout(this.editor);
+
+    // Game implicitly begins
+};
+
+Level2.prototype.loop = function() {
+    if (this.display.equals(this.editor)) {
+        this.progress++;
+        this.set_up_level();
+    }
+};
+
+Level2.prototype.win = function() {
+    this.editor.render();  // Render one more time to reset coloring
+    game.stop();
+
+    show_modal('Congratulations!',
+               'You passed a level.',
+               level_data[this.num].msg_win,
+               'Continue',
+               function(e) {console.log('not implemented');}
+              );
+    // TODO: allow advancing to the next level
+};
+
+
+// Temporary hacks while we restructure the main game logic
+function start_level_2() {
+    game = new Game(Level1);
+    game.start();
+}
+
+
 
 
 /*
