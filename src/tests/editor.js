4686c5cab43dc06de8175a281d5c35bca458154a
Kristina Fischer <kmfischer3@wisc.edu>
2016-11-19 18:48:14 -0600

    adding tests for tab

--- src/tests/editor.js
+++ src/tests/editor.js
@@ -166,6 +166,92 @@ describe('test Editor', function() {
         expect(editor.cy).toBe(4);
     });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    it('test tab at beginning of line', function() {
+        var editor = new Editor(null);
+        editor.set_data_buffer(['hello']);
+
+        editor.move_cursor(0, 0);
+        editor.type_tab();
+
+        expect(editor.data_buffer).toEqual(['  hello']);
+
+        expect(editor.cx).toBe(2);
+        expect(editor.cy).toBe(0);
+    });
+
+    it('test tab in middle of line', function() {
+        var editor = new Editor(null);
+        editor.set_data_buffer(['helloworld']);
+
+        editor.move_cursor(5, 0);
+        editor.type_tab();
+
+        expect(editor.data_buffer).toEqual(['hello  world']);
+        expect(editor.cx).toBe(7);
+        expect(editor.cy).toBe(0);
+    });
+
+    it('test tab at end of line', function() {
+        var editor = new Editor(null);
+        editor.set_data_buffer(['hello']);
+
+        editor.move_cursor(5, 0);
+        editor.type_tab();
+
+        expect(editor.data_buffer).toEqual(['hello  ']);
+
+        expect(editor.cx).toBe(7);
+        expect(editor.cy).toBe(0);
+    });
+
+    it('several tabs', function() {
+        var editor = new Editor(null);
+        editor.set_data_buffer(['hello']);
+
+        editor.move_cursor(1, 0);
+        editor.type_tab();
+        editor.type_tab();
+        editor.type_tab();
+
+        expect(editor.data_buffer).toEqual(['h      ello']);
+
+        expect(editor.cx).toBe(7);
+        expect(editor.cy).toBe(0);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     it('test backspace', function() {
         var editor = new Editor(null);
         editor.set_data_buffer(['hello']);
